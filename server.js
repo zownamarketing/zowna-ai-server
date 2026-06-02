63171c40e992cee153a241dbcaff16a92a5f0eda
zownamarketing <zowna.marketing@gmail.com>
2026-06-02 15:18:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.post("/chat", async (req, res) => {
     const userMessage = req.body.message;
 
    const response = await axios.post(
-  `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
+  `https://generativelanguage.googleapis.com/v1/models/gemini-1.5-flash-latest:generateContent?key=${process.env.GEMINI_API_KEY}`,
       {
         contents: [
           {
